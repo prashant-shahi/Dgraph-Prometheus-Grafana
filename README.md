c05cdbececc67e86b157054b50d61c99fbab2234
Prashant Shahi <prashantshahi567@gmail.com>
2019-07-23 20:23:56 +0530

    Prometheus setup

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Dgraph Prometheus
+
+Follow the steps below to setup prometheus for your Dgraph cluster :
+
+- Clone this repository and change the directory.
+```sh
+git clone https://github.com/prashant-shahi/dgraph-prometheus.git
+
+cd dgraph-prometheus
+```
+- Open **configrations.json** in your favourite text editor and modify the file if required.
+```sh
+vim configrations.json
+```
+- Make `install.sh` file executable and run.
+```sh
+chmod +x install.sh
+
+sudo ./install.sh
+```
+
+*Note:*
+- *For Dgraph zero, 6080 is the default http endpoint exposing metrics.*
+- *For Dgraph alpha, 8080 is the default http endpoint exposing metrics.*
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+PROMETHEUS_VERSION="2.11.1"
+
+wget https://github.com/prometheus/prometheus/releases/download/v2.11.1/prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz
+tar -xzvf prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz
+sudo mv prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz /opt/
+sudo ln -s /opt/prometheus-$PROMETHEUS_VERSION.linux-amd64/prometheus /usr/local/bin/
+
+if [ -e configurations.json ]
+then
+	CONFIGURATION=$(cat ./configurations.json)
+    JOB=$(echo $CONFIGURATION | python -c 'import sys, json; print json.load(sys.stdin)["job_name"]')
+    METRICS_PATH=$(echo $CONFIGURATION | python -c 'import sys, json; print json.load(sys.stdin)["metrics_path"]')
+    SCRAPE_INTERVAL=$(echo $CONFIGURATION | python -c 'import sys, json; print json.load(sys.stdin)["scrape_interval"]')
+    ENDPOINTS=$(echo $CONFIGURATION | python -c 'import sys, json; endpoints=json.load(sys.stdin)["endpoints"]; print([x.encode("utf-8") for x in endpoints])')
+else
+	JOB=dgraph
+    METRICS_PATH="/debug/prometheus_metrics"
+    SCRAPE_INTERVAL="2s"
+    ENDPOINTS="["localhost:6080","localhost:8080"]"
+fi
+
+echo "# my global config
+global:
+  scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+  # scrape_timeout is set to the global default (10s).
+
+# A scrape configuration containing exactly one endpoint to scrape:
+scrape_configs:
+">prometheus.yml
+
+echo "
+  # The job name is set to Dgraph.
+  - job_name: $JOB
+">>prometheus.yml
+
+echo "
+    # The metrics_path is set to '/debug/prometheus_metrics' than the default '/metrics'.
+    metrics_path: $METRICS_PATH
+">>prometheus.yml
+
+echo "
+    # Scraping interval is set to 2 seconds.
+    scrape_interval: $SCRAPE_INTERVAL
+">>prometheus.yml
+
+echo "
+    static_configs:
+    - targets: $ENDPOINTS
+">>prometheus.yml
+
+sudo mv prometheus.yml /opt/prometheus-$PROMETHEUS_VERSION.linux-amd64/
